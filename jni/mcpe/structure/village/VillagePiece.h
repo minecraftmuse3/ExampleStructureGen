497c80d88362b54228df27899e6c8ce621bc87ae
minecraftmuse3 <minecraftmuse3@gmail.com>
2016-02-23 19:14:50 +1300

    Update VillagePiece.h
    Definitely not complete or correct. Feel free to edit.

--- jni/mcpe/structure/village/VillagePiece.h
+++ jni/mcpe/structure/village/VillagePiece.h
@@ -5,4 +5,20 @@
 class VillagePiece : public StructurePiece {
 public:
 
+        VillagePiece();
+        VillagePiece(StartPiece*, int);
+   
+        virtual ~VillagePiece();
+  
+        virtual int getVillagerProfession(int);
+        virtual void addAdditionalSaveData(CompoundTag&);
+        virtual void readAdditionalSaveData(CompoundTag&);
+        virtual void spawnVillagers(BlockSource*, BoundingBox const&, int, int, int, int);
+        virtual void biomeBlock(FullBlock);
+        virtual void placeBlock(BlockSource*, FullBlock, int, int, int, BoundingBox const&);
+        virtual void generateBox(BlockSource*, BoundingBox const&, int, int, int, int, int, int, FullBlock, FullBlock, bool);
+        virtual void fillColumnDown(BlockSource*, FullBlock, int, int, int, BoundingBox const&);
+        virtual void getAverageGroundHeight(BlockSource*, BoundingBox const&);
+        virtual bool isOkBox(BoundingBox const&);
+
 };
