03997d0458a1c97d612753185cbd2d33ba00c29a
minecraftmuse3 <minecraftmuse3@gmail.com>
2016-02-23 19:15:51 +1300

    Add some indenting

--- jni/mcpe/structure/village/components/SimpleHouse.h
+++ jni/mcpe/structure/village/components/SimpleHouse.h
@@ -5,15 +5,15 @@
 class SimpleHouse : public VillagePiece {
 public:
 
-   SimpleHouse();
-   SimpleHouse(StartPiece*, int, Random&, BoundingBox const&, int);
-   SimpleHouse(StartPiece*, int, Random&, BoundingBox const&, int);
+        SimpleHouse();
+        SimpleHouse(StartPiece*, int, Random&, BoundingBox const&, int);
+        SimpleHouse(StartPiece*, int, Random&, BoundingBox const&, int);
    
-   virtual ~SimpleHouse();
+        virtual ~SimpleHouse();
    
-   virtual void addAdditionalSaveData(CompoundTag&);
-   virtual void readAdditionalSaveData(CompoundTag&);
-   virtual void postProcessMobsAt(BlockSource*, Random&, BoundingBox const&);
-   virtual void postProcess(BlockSource*, Random&, BoundingBox const&);
+        virtual void addAdditionalSaveData(CompoundTag&);
+        virtual void readAdditionalSaveData(CompoundTag&);
+        virtual void postProcessMobsAt(BlockSource*, Random&, BoundingBox const&);
+        virtual void postProcess(BlockSource*, Random&, BoundingBox const&);
    
 };
