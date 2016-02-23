4d79e0fad8d75f1eeeeee9f3316d84e699116927
minecraftmuse3 <minecraftmuse3@gmail.com>
2016-02-23 19:26:48 +1300

    Update StructurePiece.h

--- jni/mcpe/structure/StructurePiece.h
+++ jni/mcpe/structure/StructurePiece.h
@@ -1,11 +1,22 @@
 #pragma once
 
+class BlockID;
+class BlockSource;
+class BoundingBox;
+class FullBlock;
+
 class StructurePiece {
 public:
-
-    virtual void postProcess(BlockSource*, Random&, BoundingBox const&);
-    virtual void postProcessMobsAt(BlockSource*, Random&, BoundingBox const&);
-    virtual void placeBlock(BlockSource*, FullBlock, int, int, int, BoundingBox const&);
-    virtual void generateBox(BlockSource*, BoundingBox const&, int, int, int, int, int, int, FullBlock, FullBlock):
+        virtual ~StructurePiece();
+        
+        void generateBox(BlockSource*, BoundingBox const&, int, int, int, int, int, int, BlockID, unsigned char, BlockID, unsigned char, bool);
+        int getOrientationData(Block*, unsigned char);
+        void postProcessMobsAt(BlockSource*, Random&, BoundingBox const&);
+        void placeBlock(BlockSource*, FullBlock, int, int, int, BoundingBox const&);
+        void generateBox(BlockSource*, BoundingBox const&, int, int, int, int, int, int, FullBlock, FullBlock, bool);
+        void fillColumnDown(BlockSource*, FullBlock, int, int, int, BoundingBox const&);
+        void generateAirColumnUp(BlockSource*, int, int, int, BoundingBox const&);
+        void createDoor(BlockSource*, BoundingBox const&, Random&, int, int, int, int);
+        void generateAirBox(BlockSource*, BoundingBox const&, int, int, int, int, int, int);
     
 };
