46a4d7dd2d80e9203e88b77697435b7244c48061
minecraftmuse3 <minecraftmuse3@gmail.com>
2016-02-27 18:14:48 +1300

    Update StructurePiece.h

--- jni/mcpe/structure/StructurePiece.h
+++ jni/mcpe/structure/StructurePiece.h
@@ -9,14 +9,7 @@ class StructurePiece {
 public:
         virtual ~StructurePiece();
         
-        void generateBox(BlockSource*, BoundingBox const&, int, int, int, int, int, int, BlockID, unsigned char, BlockID, unsigned char, bool);
-        int getOrientationData(Block*, unsigned char);
-        virtual void postProcessMobsAt(BlockSource*, Random&, BoundingBox const&);
         void placeBlock(BlockSource*, FullBlock, int, int, int, BoundingBox const&);
         void generateBox(BlockSource*, BoundingBox const&, int, int, int, int, int, int, FullBlock, FullBlock, bool);
-        void fillColumnDown(BlockSource*, FullBlock, int, int, int, BoundingBox const&);
-        void generateAirColumnUp(BlockSource*, int, int, int, BoundingBox const&);
-        void createDoor(BlockSource*, BoundingBox const&, Random&, int, int, int, int);
-        void generateAirBox(BlockSource*, BoundingBox const&, int, int, int, int, int, int);
     
 };
