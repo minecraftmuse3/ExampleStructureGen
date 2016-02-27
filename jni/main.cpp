503ef181c49c1792974c16c2f1d50cc7db267d60
Răzvan <razvan-mccrafter@outlook.com>
2016-02-27 23:10:11 +0200

    Fixed all bugs but...
    But crashes when generating a village

--- jni/main.cpp
+++ jni/main.cpp
@@ -8,12 +8,11 @@
 #include "mcpe/block/Block.h"
 #include "mcpe/structure/village/components/SimpleHouse.h"
 
-#define LOG_TAG "examplevillagegen"
-
+#define LOG_TAG "ExampleVillageGen"
 #define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__))
 
 void (*_SimpleHouse$postProcess)(SimpleHouse* self, BlockSource*, Random&, BoundingBox const&);
-void SimpleHouse$postProcess(SimpleHouse* self*, BlockSource* region, Random& rand, BoundingBox const& bounds) {
+void SimpleHouse$postProcess(SimpleHouse* self, BlockSource* region, Random& rand, BoundingBox const& bounds) {
 	self->placeBlock(region, {Block::mCobblestone->blockId, 0}, 0, 1, 0, bounds);
 	self->generateBox(region, bounds, 0, 0, 0, 5, 5, 5, {Block::mStone->blockId, 0}, {Block::mStone->blockId, 0}, true);
 }
@@ -21,7 +20,8 @@ void SimpleHouse$postProcess(SimpleHouse* self*, BlockSource* region, Random& ra
 JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
 	
 	void* handle = dlopen("libminecraftpe.so", RTLD_LAZY);
-	void* SimpleHouse$postProcess_ = dlsym(handle, "_ZN11SimpleHouse11postProcessEP11BlockSourceR6RandomRK11BoundingBox")
-        MSHookFunction(SimpleHouse$postProcess_, (void*) &SimpleHouse$postProcess, (void**) &_SimpleHouse$postProcess);
+	void* SimpleHouse$postProcess_ = dlsym(handle, "_ZN11SimpleHouse11postProcessEP11BlockSourceR6RandomRK11BoundingBox");
+    MSHookFunction(SimpleHouse$postProcess_, (void*) &SimpleHouse$postProcess, (void**) &_SimpleHouse$postProcess);
+
 	return JNI_VERSION_1_2;
 }
