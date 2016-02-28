cfca2ca1cbfbeb2bbdd16425c7760cb22c12ff71
minecraftmuse3 <minecraftmuse3@gmail.com>
2016-02-28 18:13:33 +1300

    Update VillagePiece.h

--- jni/mcpe/structure/village/VillagePiece.h
+++ jni/mcpe/structure/village/VillagePiece.h
@@ -21,8 +21,8 @@ public:
         virtual void readAdditionalSaveData(CompoundTag&);
         virtual void spawnVillagers(BlockSource*, BoundingBox const&, int, int, int, int);
         virtual void biomeBlock(FullBlock);
-        virtual void placeBlock(BlockSource*, FullBlock, int, int, int, BoundingBox const&);
-        virtual void generateBox(BlockSource*, BoundingBox const&, int, int, int, int, int, int, FullBlock, FullBlock, bool);
+        void placeBlock(BlockSource*, FullBlock, int, int, int, BoundingBox const&);
+        void generateBox(BlockSource*, BoundingBox const&, int, int, int, int, int, int, FullBlock, FullBlock, bool);
         virtual void fillColumnDown(BlockSource*, FullBlock, int, int, int, BoundingBox const&);
         virtual void getAverageGroundHeight(BlockSource*, BoundingBox const&);
         virtual bool isOkBox(BoundingBox const&);
