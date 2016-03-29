1d7f12388e9dff37eac6e0c21d745da45d0a88be
Răzvan <razvan-mccrafter@outlook.com>
2016-03-29 21:07:02 +0300

    Some fixes

--- jni/mcpe/structure/StructurePiece.h
+++ jni/mcpe/structure/StructurePiece.h
@@ -9,11 +9,13 @@ class Random;
 class BlockSelector;
 class ChunkPos;
 class Brightness;
+class WeighedTreasureItem;
 
 class StructurePiece {
 public:
 	virtual ~StructurePiece();
 
+	void addChildren(StructurePiece*, std::vector<std::unique_ptr<StructurePiece, std::default_delete<StructurePiece>>, std::allocator<std::unique_ptr<StructurePiece, std::default_delete<StructurePiece>>>>&, Random&);
 	void _getWorldPos(int, int, int);
 	void getWorldX(int, int);
 	void getWorldY(int);
@@ -25,7 +27,7 @@ public:
 	void getTorchData(unsigned char);
 	FullBlock getBlock(BlockSource*, int, int, int, BoundingBox const&);
 	bool isAir(BlockSource*, int, int, int, BoundingBox const&);
-	//void createChest(BlockSource*, BoundingBox const&, Random&, int, int, int, int, std::vector<WeighedTreasureItem, std::allocator<WeighedTreasureItem>> const&, int);
+	void createChest(BlockSource*, BoundingBox const&, Random&, int, int, int, int, std::vector<WeighedTreasureItem, std::allocator<WeighedTreasureItem>> const&, int);
 	void createDoor(BlockSource*, BoundingBox const&, Random&, int, int, int, int);
 	void edgesLiquid(BlockSource*, BoundingBox const&);
 	void fillColumnDown(BlockSource*, FullBlock, int, int, int, BoundingBox const&);
