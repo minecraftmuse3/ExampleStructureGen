9431f3a1dac3dce48821acbbb745331ef03588e0
minecraftmuse3 <minecraftmuse3@gmail.com>
2016-04-20 20:58:52 +1200

    Create SimpleHouse.cpp

---
+++ jni/villagegen/level/structure/village/component/SimpleHouse.cpp
@@ -0,0 +1,10 @@
+#include "mcpe/level/structure/village/components/SimpleHouse/h"
+
+include "mcpe/level/block/Block.h"
+
+void SimpleHouse::_postProcess(BlockSource* region, Random& rand, BoundingBox const& bounds) {
+        //We generate a basic cube
+	this->generateBox(region, bounds, 0, 0, 0, 4, 4, 4, {Block::mBrick->blockId, 0}, {Block::mAir->blockId, 0}, false);
+	this->createDoor(region, bounds, random, 2, 1, 0, 0);
+}
+       
