5da2059b8af02dce5507f4cfb8c7c5252c6fbadb
Răzvan <razvan.filea@gmail.com>
2016-05-11 15:30:27 +0300

    Quick fix

--- jni/mcpe/level/structure/StartPiece.h
+++ jni/mcpe/level/structure/StartPiece.h
@@ -12,7 +12,7 @@ public:
 	StartPiece(BiomeSource*, int, Random&, int, int, std::vector<PieceWeight, std::allocator<PieceWeight>>&, int);
 
 	virtual ~StartPiece();
-	//virtual void addChildren(); //It's to hard to demangle
+	virtual void addChildren(StructurePiece*, std::vector<std::unique_ptr<StructurePiece, std::default_delete<StructurePiece>>, std::allocator<std::unique_ptr<StructurePiece, std::default_delete<StructurePiece>>>>&, Random&);
 	void getPieceWeights();
 	void getBiomeSource();
 };
